0cc1d098099a7f0a3a4f1b4862a922123831f9ad
Nikola Misic <nikola.misic@bitgrip.de>
2025-02-12 19:32:44 +0100

    Remove preloader

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
 //loader
-paceOptions = {
-  ajax: true,
-  document: true,
-  eventLag: false
-  };
+// paceOptions = {
+//   ajax: true,
+//   document: true,
+//   eventLag: false
+//   };
 
-  Pace.on('done', function() {
+//   Pace.on('done', function() {
 
-  $('#preloader').delay(0).animate({top: '-120%'}, 3800, $.bez([0.19,1,0.22,1]));
+//   $('#preloader').delay(0).animate({top: '-120%'}, 3800, $.bez([0.19,1,0.22,1]));
 
 
- });
+//  });
 
 $(window).on('load',function(){
   $(function(){
